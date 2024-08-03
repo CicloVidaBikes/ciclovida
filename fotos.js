909d6a116e3ec3a5831150c5a5dd5b8ccee3f2c1
CicloVidaBikes <164611677+CicloVidaBikes@users.noreply.github.com>
2024-08-03 00:28:09 -0600

    Add files via upload

---
+++ fotos.js
@@ -0,0 +1,54 @@
+const slider = document.querySelector("#slider");
+let sliderSection = document.querySelectorAll(".slider__section");
+let sliderSectionLast = sliderSection[sliderSection.length -1];
+
+const btnLeft = document.querySelector("#btn-left");
+const btnRight = document.querySelector("#btn-right");
+
+slider.insertAdjacentElement('afterbegin', sliderSectionLast);
+
+function Next(){
+    let sliderSectionFirst = document.querySelectorAll(".slider__section")[0];
+    slider.style.marginLeft = "-200%";
+    slider.style.transition = "all 0.5s";
+    setTimeout(function(){
+        slider.style.transition = "none";
+        slider.insertAdjacentElement('beforeend', sliderSectionFirst);
+        slider.style.marginLeft = "-100%";
+    }, 500);
+}
+
+function Prev(){
+    let sliderSection = document.querySelectorAll(".slider__section");
+    let sliderSectionLast = sliderSection[sliderSection.length -1];
+    slider.style.marginLeft = "0";
+    slider.style.transition = "all 0.5s";
+    setTimeout(function(){
+        slider.style.transition = "none";
+        slider.insertAdjacentElement('afterbegin', sliderSectionLast);
+        slider.style.marginLeft = "-100%";
+    }, 500);
+}
+
+btnRight.addEventListener('click', function(){
+    Next();
+});
+
+btnLeft.addEventListener('click', function(){
+    Prev();
+});
+
+setInterval(function(){
+    Next();
+}, 5000)
+
+function cambiarColor(boton) {
+    // Obtener todos los botones con la misma clase
+    var botones = document.querySelectorAll("." + boton.classList[1]);
+    // Quitar la clase "boton-activo" de todos los botones
+    botones.forEach(function(boton) {
+        boton.classList.remove("boton-activo");
+    });
+    // Agregar la clase "boton-activo" solo al botón presionado
+    boton.classList.add("boton-activo");
+}
